389064797f50d2b992612ba560f70fe6e452f1bb
DieterVyncke <vynckedieter@gmail.com>
2015-12-17 18:57:25 +0100

    Initial commit

---
+++ Beered/Beered/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beered
+//
+//  Created by Dieter Vyncke on 17/12/15.
+//  Copyright © 2015 Dieter Vyncke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
